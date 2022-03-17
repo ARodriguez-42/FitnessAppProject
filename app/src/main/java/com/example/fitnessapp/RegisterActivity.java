7f39b2f90892ad4731f2c53951966e203241a537
antwanthegreat <rodriguez42antonio@gmail.com>
2022-03-17 13:06:52 -0700

    Initial Commit

--- app/src/main/java/com/example/fitnessapp/RegisterActivity.java
+++ app/src/main/java/com/example/fitnessapp/RegisterActivity.java
@@ -5,17 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -24,8 +31,9 @@ public class RegisterActivity extends AppCompatActivity {
     TextInputEditText etRegPassword;
     TextView tvLoginHere;
     Button btnRegister;
-
+    String userID;
     FirebaseAuth mAuth;
+    FirebaseFirestore firestore;
 
 
     @Override
@@ -40,7 +48,7 @@ public class RegisterActivity extends AppCompatActivity {
         btnRegister = findViewById(R.id.btnRegister);
 
         mAuth = FirebaseAuth.getInstance();
-
+        firestore = FirebaseFirestore.getInstance();
         btnRegister.setOnClickListener(view ->{
             createUser();
         });
@@ -53,6 +61,7 @@ public class RegisterActivity extends AppCompatActivity {
     private void createUser(){
         String email = etRegEmail.getText().toString();
         String password = etRegPassword.getText().toString();
+        String username = etRegUserName.getText().toString();
 
         if (TextUtils.isEmpty(email)){
             etRegEmail.setError("Email cannot be empty");
@@ -66,6 +75,18 @@ public class RegisterActivity extends AppCompatActivity {
                 public void onComplete(@NotNull Task<AuthResult> task) {
                     if (task.isSuccessful()){
                         Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                        userID = mAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference = firestore.collection("users").document(userID);
+                        Map<String, Object> user = new HashMap<>();
+                        user.put("Username", username);
+                        user.put("Email", email);
+                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.d("TAG", "onSuccess: User Profile is created for " + userID);
+
+                            }
+                        });
                         startActivity(new Intent(RegisterActivity.this, MainActivity.class));
                     }else{
                         Toast.makeText(RegisterActivity.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
