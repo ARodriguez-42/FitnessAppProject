e48e45b9b949c58bef71c98399f1aee49c324655
antwanthegreat <rodriguez42antonio@gmail.com>
2022-03-31 20:17:19 -0700

    Initial Commit

--- app/src/main/java/com/example/fitnessapp/ui/home/AddSets.java
+++ app/src/main/java/com/example/fitnessapp/ui/home/AddSets.java
@@ -6,11 +6,18 @@ import android.os.Bundle;
 
 import com.example.fitnessapp.R;
 
+import java.util.ArrayList;
+
 public class AddSets extends AppCompatActivity {
+
+    ArrayList<Set> set;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_sets);
+
+
+
     }
 }
--- app/src/main/java/com/example/fitnessapp/ui/home/AddSetsAdapter.java
+++ app/src/main/java/com/example/fitnessapp/ui/home/AddSetsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.fitnessapp.ui.home;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.fitnessapp.Exercise;
 import com.example.fitnessapp.R;
 import com.example.fitnessapp.RecyclerViewInterface;
 
@@ -28,12 +30,15 @@ public class AddSetsAdapter extends RecyclerView.Adapter<AddSetsAdapter.MyViewHo
     @NonNull
     @Override
     public AddSetsAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(context).inflate(R.layout.add_set_item, parent, false);
+        return new AddSetsAdapter.MyViewHolder(v, recyclerViewInterface);
     }
 
     @Override
     public void onBindViewHolder(@NonNull AddSetsAdapter.MyViewHolder holder, int position) {
-
+        Set set = list.get(position);
+        holder.weightIN.setText(set.getWeight());
+        holder.repIN.setText(set.getReps());
     }
 
     @Override
@@ -43,13 +48,13 @@ public class AddSetsAdapter extends RecyclerView.Adapter<AddSetsAdapter.MyViewHo
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
 
-        TextView exerciseName;
-
+        TextView weightIN, repIN;
 
         public MyViewHolder(@NonNull View itemView, RecyclerViewInterface recyclerViewInterface) {
             super(itemView);
 
-            exerciseName = itemView.findViewById(R.id.displayExercise);
+            weightIN = itemView.findViewById(R.id.weightIN);
+            repIN = itemView.findViewById(R.id.repIN);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
